73c8c8ff472d07f4bc5db1c6f723971c1670c5b0
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 12:08:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -59,15 +59,15 @@ async function searchRestaurants() {
     console.log("search",tags["name"],tags["addr:housenumber"],tags["addr:street"],tags["addr:postcode"])
     //const app = express();
     //app.get("/get-html", async (req, res) => {
-      try {
-        const response = await fetch(`https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}`);
-        const html = await response.text();
-        console.log("html",html)
-    //    res.send(html);
-    //  }
-    //  catch (err) {
-    //    res.status(500).send("Error fetching HTML");
-    //  }
+    //try {
+      //const response = await fetch(`https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}`);
+      //const html = await response.text();
+      //console.log("html",html)
+      //res.send(html);
+      //}
+      //catch (err) {
+      //  res.status(500).send("Error fetching HTML");
+      //}
     //});
     //app.listen(3000, () => console.log("Running.."));
     //const res = await fetch(`http://localhost:3000/get-html`);
