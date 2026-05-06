919295f91c70bed5751bcc7be8773a0cc3cceb2b
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 00:02:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,11 @@ async function searchRestaurants() {
     });
   console.log("Res",res)
   const data = await res.json();
-  console,log("tags.name",tags.name)
+  console.log("tags.name",tags.name)
+  console.log("tags.addr:housenumber",tags.addr:housenumber)
+  console.log("tags..addr:street",tags..addr:street)
+  console.log("tags.tags.addr:postcode",tags.tags.addr:postcode)
+    
   //console.log("tags.name",tags.name,"tags.addr:housenumber",tags.addr:housenumber,"tags.addr:street",tags.addr:street,"tags.addr:postcode",tags.addr:postcode)
   console.log("Data",data);
   list.innerHTML = "";
