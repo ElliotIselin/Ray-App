a5a71e1dc30a7edfd51e9cfea6495c4f85b65740
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 00:59:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ async function searchRestaurants() {
     //  catch (err) {
     //    res.status(500).send("Error fetching HTML");
     //  }
-    });
+    //});
     app.listen(3000, () => console.log("Running.."));
     //const res = await fetch(`http://localhost:3000/get-html`);
     //const html = await res.test();
