dd4cdb377629bc08f2b7d8c5afd23d1df9d105fe
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 00:56:10 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+app.get("/get-html", async (req, res) => {
+  try {
+    const response = await fetch(`https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}`);
+    const html = await response.text();
+    console.log("html",html)
+    res.send(html);
+  }
+  catch (err) {
+    res.status(500).send("Error fetching HTML");
+  }
+});
+app.listen(3000, () => console.log("Running.."));
