93b7fc6c3dec2c531fccaad1868ce3f2823e03c7
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 12:08:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from "express";
+//import express from "express";
 //import fetch from "node-fetch";
-const app = express();
-app.get("/get-html", async (req, res) => {
-  try {
-    const response = await fetch(`https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}`);
-    const html = await response.text();
-    console.log("html",html)
-    res.send(html);
-  }
-  catch (err) {
-    res.status(500).send("Error fetching HTML");
-  }
-});
-app.listen(3000, () => console.log("Running.."));
+//const app = express();
+//app.get("/get-html", async (req, res) => {
+//  try {
+//    const response = await fetch(`https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}`);
+//    const html = await response.text();
+//    console.log("html",html)
+//    res.send(html);
+//  }
+//  catch (err) {
+//    res.status(500).send("Error fetching HTML");
+//  }
+//});
+//app.listen(3000, () => console.log("Running.."));
