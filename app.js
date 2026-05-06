025481c67f624e428bbd3f124792cc717dad2b23
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 00:39:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ async function searchRestaurants() {
     const app = express();
     app.get("/get-html", async (req, res) => {
       try {
-        const response = await fetch("https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+{tags["addr:postcode"]}");
+        const response = await fetch("https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}");
         const html = await response.text();
         console.log("html",html)
         res.send(html);
