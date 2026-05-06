fd43b024e6df4be53359f2d626d036ef5590dd82
ElliotIselin <Iselin.Elliot@gmail.com>
2026-05-06 12:01:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-//import express from "express";
+import express from "express";
 //import fetch from "node-fetch";
+const app = express();
 app.get("/get-html", async (req, res) => {
   try {
     const response = await fetch(`https://google.com/search?q=${tags["name"]}+${tags["addr:housenumber"]}+${tags["addr:street"]}+${tags["addr:postcode"]}`);
